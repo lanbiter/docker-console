41ba1c545f457c8ee58fdbe4fa8a04ff371820c6
lanbiter <15839114763@163.com>
2017-02-06 07:24:49 -0500

    init repo

---
+++ static/dist/main.js
@@ -0,0 +1,99 @@
+var term,
+    protocol,
+    socketURL,
+    socket,
+    pid,
+    charWidth,
+    charHeight;
+
+var terminalContainer = document.getElementById('terminal-container');
+
+function setTerminalSize () {
+  var cols = 80,
+      rows = 24,
+      width = '1000px',
+      height ='500px';
+
+  terminalContainer.style.width = width;
+  terminalContainer.style.height = height;
+  term.resize(cols, rows);
+}
+
+createTerminal();
+
+function createTerminal() {
+  // Clean terminal
+  while (terminalContainer.children.length) {
+    terminalContainer.removeChild(terminalContainer.children[0]);
+  }
+  term = new Terminal({
+    cursorBlink: false,
+    scrollback: 1000,
+    tabStopWidth: 8
+  });
+  term.open(terminalContainer);
+  term.fit();
+
+  var initialGeometry = term.proposeGeometry(),
+      cols = 80,
+      rows = 24;
+
+  containerId = '3b5e25b3f62d';
+  // socketURL = 'ws://100.73.35.8:2375/v1.24/containers/' + containerId + '/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1';
+  socketURL = 'ws://100.73.35.8:5000/echo';
+  console.log(socketURL)
+  socket = new WebSocket(socketURL);
+  socket.onopen = runRealTerminal;
+      socket.onclose = runFakeTerminal;
+      socket.onerror = runFakeTerminal;
+      socket.onmessage = function (e) {
+              console.log(e);
+              console.warn(e.data);
+  }
+}
+
+function runRealTerminal() {
+  term.attach(socket);
+  term._initialized = true;
+}
+
+function runFakeTerminal() {
+  if (term._initialized) {
+    return;
+  }
+
+  term._initialized = true;
+
+  var shellprompt = '$ ';
+
+  term.prompt = function () {
+    term.write('\r\n' + shellprompt);
+  };
+
+  term.writeln('Welcome to xterm.js');
+  term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
+  term.writeln('Type some keys and commands to play around.');
+  term.writeln('');
+  term.prompt();
+
+  term.on('key', function (key, ev) {
+    var printable = (
+      !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+    );
+
+    if (ev.keyCode == 13) {
+      term.prompt();
+    } else if (ev.keyCode == 8) {
+     // Do not delete the prompt
+      if (term.x > 2) {
+        term.write('\b \b');
+      }
+    } else if (printable) {
+      term.write(key);
+    }
+  });
+
+  term.on('paste', function (data, ev) {
+    term.write(data);
+  });
+}
